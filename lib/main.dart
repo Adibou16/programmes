58da419bdbbd01cf9588dc26ba56eca8279a8f65
Félix Albert <filalbert16@gmail.com>
2025-10-29 21:26:32 -0400

    Adding images to box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:programmes/database/boxes.dart';
@@ -7,6 +6,8 @@ import 'package:programmes/database/images.dart';
 import 'package:programmes/database/workout.dart';
 import 'package:programmes/pages/navigation.dart';
 import 'package:programmes/workout/exercise/exercise_card.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
@@ -17,14 +18,36 @@ Future<void> main() async {
   boxImages = await Hive.openBox<Images>('imagesBox');
 
   if (boxImages.isEmpty) {
-    print("Empty");
-    var imgDir = await Directory('assets/exercise_images').create(recursive: true);
+    final appDir = await getApplicationDocumentsDirectory();
+    final imgDir = Directory('${appDir.path}/exercise_images');
 
-    if (imgDir.existsSync()) {
-      print("Exists");
-    } else {
-      print("FUCK");
+    final assetManifest = await AssetManifest.loadFromAssetBundle(rootBundle);
+    final imagePaths = assetManifest
+      .listAssets()
+      .where((path) => path.startsWith('assets/exercise_images/'))
+      .toList();
+  
+    for (String assetPath in imagePaths) {
+      final fileName = assetPath.split('/').last;
+      final byteData = await rootBundle.load(assetPath);
+      final file = File('${imgDir.path}/$fileName');
+      await file.writeAsBytes(byteData.buffer.asUint8List());
     }
+
+      if (imgDir.existsSync()) {
+        final files = imgDir.listSync(recursive: false, followLinks: false);
+
+        for (var file in files) {
+          final String fileName = file.path.split('/').last;
+
+          if (file is File) {
+            final imgBytes = await file.readAsBytes();
+            boxImages.put('key_$fileName', Images(name: fileName, image: imgBytes));
+          }
+        }
+      } else {
+        print("ERROR CREATING IMAGES");
+      }
   }
 
   runApp(MaterialApp(
